c2df728fdc3125fcbdb071af7fd2c85301bf33a5
Tuna <anhjack1102@gmail.com>
2025-11-29 19:13:26 +0700

    init

---
+++ src/hooks/useSocket.ts
@@ -0,0 +1,18 @@
+// src/hooks/useSocket.ts
+import { useEffect, useState } from "react";
+import { io, Socket } from "socket.io-client";
+
+export const useSocket = (url: string) => {
+  const [socket, setSocket] = useState<Socket | null>(null);
+
+  useEffect(() => {
+    const newSocket = io(url); // kết nối tới server NestJS
+    setSocket(newSocket);
+
+    return () => {
+      newSocket.disconnect();
+    };
+  }, [url]);
+
+  return socket;
+};
